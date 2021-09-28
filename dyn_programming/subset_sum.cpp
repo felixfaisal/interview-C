5d4031ab0c6e4fdb9868423405ecec27eb0cc211
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-09-28 18:20:31 +0530

    Implemeneted top down for subset sum

--- dyn_programming/subset_sum.cpp
+++ dyn_programming/subset_sum.cpp
@@ -22,10 +22,38 @@ bool subset(int wt[], int sum, int n){
         return arr[n][sum]=subset(wt,sum,n-1);
     }
 }
+bool subset_topdown(int wt[], int sum, int n){
+    bool t[n+1][sum+1];
+    //initialization
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=sum;j++){
+            if(i==0){
+                t[i][j] = false;
+            }
+            if(j==0){
+                t[i][j] = true;
+            }
+        }
+    }
+    // Choice diagram
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+            if(wt[i-1]<=j){
+                t[i][j] = t[i-1][j-wt[i-1]] || t[i-1][j];
+            }
+            else{
+                t[i][j] = t[i-1][j];
+            }
+        }
+    }
+    return t[n][sum];
+}
 int main(){
     int sum = 10;
     int wt[] = {5,3,2,6};
     memset(arr,-1,sizeof(arr));
-    int n = sizeof(wt)/sizeof(int);
+    int n = sizeof(wt)/sizeof(wt[0]);
     cout<<subset(wt,sum,n)<<endl;
+    cout<<subset_topdown(wt,sum,n)<<endl;
+    return 0;
 }
